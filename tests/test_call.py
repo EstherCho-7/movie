f2431f2411e4f1f1ea364ecfe2ce15d3ebf15a72
EstherCho-7 <dkgkqm777@gmail.com>
2024-07-26 16:29:46 +0900

    add test

---
+++ tests/test_call.py
@@ -0,0 +1,30 @@
+from mov.api.call import gen_url, req, get_key, req2dataframe
+
+def test_private_key():
+    key=get_key()
+    assert key
+
+def test_gen_url(): 
+    url=gen_url()
+    assert True
+    assert "http" in url
+    assert "kobis" in url
+
+def test_req():
+    code, data=req()
+    assert code == 200
+
+    code, data=req("20230101")
+    assert code == 200
+
+def test_dataframe():
+    l=req2dataframe()
+    assert len(l)>0
+    v=l[0]
+    assert 'rnum' in v.keys() 
+    assert v['rnum']=='1'
+   # dataframe=req2dataframe()
+   # if len(dataframe)>=1:
+   #     print(dataframe)
+   # else:
+   #     print("Nope")
